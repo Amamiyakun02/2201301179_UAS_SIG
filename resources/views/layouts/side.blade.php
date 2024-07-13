9748f132c477c4a0dd287a7fdb89b12c5a2b225d
Amamiyakun02 <amamiyakun02@gmail.com>
2024-07-13 15:50:12 +0800

    routes

---
+++ resources/views/layouts/side.blade.php
@@ -0,0 +1,90 @@
+<aside class="left-sidebar" data-sidebarbg="skin6">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar" data-sidebarbg="skin6">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="index.html"
+                        aria-expanded="false"><i data-feather="home" class="feather-icon"></i><span
+                            class="hide-menu">Dashboard</span></a></li>
+                <li class="list-divider"></li>
+                <li class="nav-small-cap"><span class="hide-menu">Aplikasi</span></li>
+
+                <li class="sidebar-item"> <a class="sidebar-link" href="ticket-list.html"
+                        aria-expanded="false"><i data-feather="tag" class="feather-icon"></i><span
+                            class="hide-menu"> Data Perkebunan
+                        </span></a>
+                </li>
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-chat.html"
+                        aria-expanded="false"><i data-feather="message-square" class="feather-icon"></i><span
+                            class="hide-menu">Marker</span></a></li>
+                <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="app-calendar.html"
+                        aria-expanded="false"><i data-feather="calendar" class="feather-icon"></i><span
+                            class="hide-menu">Peta</span></a></li>
+
+                <li class="list-divider"></li>
+                <li class="nav-small-cap"><span class="hide-menu">Fitur</span></li>
+                <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                        aria-expanded="false"><i data-feather="file-text" class="feather-icon"></i><span
+                            class="hide-menu">Tambah Data </span></a>
+                    <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                        <li class="sidebar-item"><a href="form-inputs.html" class="sidebar-link"><span
+                                    class="hide-menu"> Tambah Perkebunan
+                                </span></a>
+                        </li>
+                        <li class="sidebar-item"><a href="form-input-grid.html" class="sidebar-link"><span
+                                    class="hide-menu"> Tambah Jenis Perkebunan
+                                </span></a>
+                        </li>
+                        <li class="sidebar-item"><a href="form-checkbox-radio.html" class="sidebar-link"><span
+                                    class="hide-menu">Tambah Marker
+                                </span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                        aria-expanded="false"><i data-feather="grid" class="feather-icon"></i><span
+                            class="hide-menu">Jenis Perkebunan</span></a>
+                    <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                        <li class="sidebar-item"><a href="table-basic.html" class="sidebar-link"><span
+                                    class="hide-menu"> Basic Table
+                                </span></a>
+                        </li>
+                        <li class="sidebar-item"><a href="table-dark-basic.html" class="sidebar-link"><span
+                                    class="hide-menu"> Dark Basic Table
+                                </span></a>
+                        </li>
+                        <li class="sidebar-item"><a href="table-sizing.html" class="sidebar-link"><span
+                                    class="hide-menu">
+                                    Sizing Table
+                                </span></a>
+                        </li>
+
+                    </ul>
+                </li>
+                <li class="sidebar-item"> <a class="sidebar-link has-arrow" href="javascript:void(0)"
+                        aria-expanded="false"><i data-feather="bar-chart" class="feather-icon"></i><span
+                            class="hide-menu">Charts </span></a>
+                    <ul aria-expanded="false" class="collapse  first-level base-level-line">
+                        <li class="sidebar-item"><a href="chart-morris.html" class="sidebar-link"><span
+                                    class="hide-menu"> Morris Chart
+                                </span></a>
+                        </li>
+                        <li class="sidebar-item"><a href="chart-chart-js.html" class="sidebar-link"><span
+                                    class="hide-menu"> ChartJs
+                                </span></a>
+                        </li>
+                        <li class="sidebar-item"><a href="chart-knob.html" class="sidebar-link"><span
+                                    class="hide-menu">
+                                    Knob Chart
+                                </span></a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="list-divider"></li>
+            </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,43 @@ use App\Http\Controllers\API\DataController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.index');
 });
 
+//Routes Kebun
+Route::prefix('kebun')->group(function () {
+    Route::get('/', [KebunController::class, 'index']);
+    Route::get('/tambah', [KebunController::class, 'create']);
+    Route::post('/store', [KebunController::class, 'store']);
+    Route::get('/edit/{id}', [KebunController::class, 'edit']);
+    Route::put('/update', [KebunController::class, 'update']);
+    Route::delete('/delete/{id}', [KebunController::class, 'destroy']);
+});
 
-Route::get('/kebun', [KebunController::class, 'index']);
-Route::get('/icon', [IconController::class, 'index']);
-Route::get('/jenis_perkebunan', [JenisPerkebunanController::class, 'index']);
-Route::get('/jenis_perkebunan/tambah', [JenisPerkebunanController::class, 'tambah'])->name('jenis_perkebunan.tambah');
+//Routes Marker
+Route::prefix('marker')->group(function () {
+    Route::get('/', [IconController::class, 'index']);
+    Route::get('/tambah', [IconController::class, 'tambah']);
+    Route::post('/store', [IconController::class, 'store']);
+    Route::get('/edit/{id}', [IconController::class, 'edit']);
+    Route::put('//update', [IconController::class, 'update']);
+    Route::delete('/delete/{id}', [IconController::class, 'destroy']);
+});
+
+//Route jenis Perkebunan
+Route::prefix('jenis-perkebunan')->group(function () {
+    Route::get('/', [JenisPerkebunanController::class, 'index']);
+    Route::get('/tambah', [JenisPerkebunanController::class, 'tambah']);
+    Route::post('/store', [JenisPerkebunanController::class, 'store']);
+    Route::get('/edit/{id}', [JenisPerkebunanController::class, 'edit']);
+    Route::put('/update', [JenisPerkebunanController::class, 'update']);
+    Route::delete('/delete/{id}', [JenisPerkebunanController::class, 'destroy']);
+});
+
+//Routes API
+Route::prefix('api')->group(function () {
+    Route::get('/data', [DataController::class, 'index']);
+});
+
 Route::get('/main', [BaseController::class, 'index']);
 
